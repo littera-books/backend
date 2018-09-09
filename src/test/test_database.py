3d171f33f803be623df910fef759dffcd1e7c68e
huskyhoochu <dfg1499@gmail.com>
2018-09-09 21:38:15 +0900

    [#2] 유저 관련 테스트 추가
    - DB 상에서 유저 생성, 삭제 테스트
    - URL 상에서 유저 생성, 삭제 테스트

--- src/test/test_database.py
+++ src/test/test_database.py
@@ -9,17 +9,37 @@ from sqlalchemy.orm import sessionmaker
 from common.database import Base
 from user.model import User
 
-class TestDB(unittest.TestCase):
-  def setUp(self):
-    self.engine = create_engine('sqlite:///:memory:', echo=True)
-    self.sessionmaker = sessionmaker(bind=self.engine)
-    self.session = self.sessionmaker()
-    self.base = Base
-    Base.metadata.create_all(bind=self.engine)
 
-  def test_user_create(self):
-    dummy_user = User(email='abc@abc.com', password='dummy')
-    self.session.add(dummy_user)
-    check_user = self.session.query(User).filter_by(email='abc@abc.com').first()
-    assert dummy_user == check_user
-    
+class TestDB(unittest.TestCase):
+    def setUp(self):
+        self.engine = create_engine('sqlite:///:memory:', echo=True)
+        self.sessionmaker = sessionmaker(bind=self.engine)
+        self.session = self.sessionmaker()
+        self.base = Base
+        self.data = {'email': 'abc@abc.com', 'password': 'dummy'}
+        Base.metadata.create_all(bind=self.engine)
+
+    def test_user_create(self):
+        """
+        DB에서 유저 생성 테스트
+        """
+        dummy_user = User(
+            email=self.data['email'], password=self.data['password'])
+        self.session.add(dummy_user)
+        query_user = self.session.query(User).filter_by(
+            email=self.data['email']).first()
+        self.assertEqual(dummy_user, query_user)
+
+    def test_user_delete(self):
+        """
+        DB에서 유저 삭제 테스트
+        """
+        dummy_user = User(
+            email=self.data['email'], password=self.data['password'])
+        self.session.add(dummy_user)
+        query_user = self.session.query(User).filter_by(
+            email=self.data['email']).first()
+        self.session.delete(query_user)
+        is_exists = self.session.query(User).filter_by(
+            email=self.data['email']).count()
+        self.assertEqual(is_exists, 0)
---
+++ src/test/test_user.py
@@ -0,0 +1,51 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+import json
+import unittest
+
+from main import APP
+
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.data = {'email': 'dummy@test.com', 'password': '1234'}
+        self.wrong_data = {'email': 'dummy@test.com', 'password': '4321'}
+
+    def tearDown(self):
+        """
+        더미 유저 삭제
+        """
+        request, response = APP.test_client.delete(
+            '/user', data=json.dumps(self.data))
+
+    def test_user_create(self):
+        """
+        url에서 유저 생성 테스트
+        """
+        request, response = APP.test_client.post(
+            '/user', data=json.dumps(self.data))
+        self.assertEqual(response.status, 201)
+        self.assertEqual(response.json.get('email'), self.data['email'])
+
+    def test_user_delete(self):
+        """
+        url에서 유저 올바른 삭제 테스트
+        """
+        equest, response = APP.test_client.post(
+            '/user', data=json.dumps(self.data))
+        request, response = APP.test_client.delete(
+            '/user', data=json.dumps(self.data))
+        self.assertEqual(response.status, 204)
+
+    def test_user_wrong_password_delete(self):
+        """
+        url에서 유저 잘못된 패스워드 삭제 테스트
+        """
+        request, response = APP.test_client.post(
+            '/user', data=json.dumps(self.data))
+        request, response = APP.test_client.delete(
+            '/user', data=json.dumps(self.wrong_data))
+        self.assertEqual(response.json.get('message'), '유저 정보가 맞지 않습니다.')
+        self.assertEqual(response.status, 400)
