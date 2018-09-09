56212069aa8771992b33d54901296d511144a7da
huskyhoochu <dfg1499@gmail.com>
2018-09-09 20:50:23 +0900

    [#2] Server Listener 사용
    - DB 매핑 메서드를 listener에 옮겨 붙임
    - 서버 스타트 메시지 추가

--- src/main.py
+++ src/main.py
@@ -9,6 +9,17 @@ APP = Sanic(__name__)
 APP.add_route(UserView.as_view(), '/user')
 
 
+@APP.listener('before_server_start')
+async def setup_db(app, loop):
+    # Base에 연결된 모든 테이블 매핑
+    Base.metadata.create_all(bind=engine)
+
+
+@APP.listener('after_server_start')
+async def notify_server_started(app, loop):
+    print('\nWelcome Littera API server\n')
+
+
 @APP.route('/')
 async def main(request):
     """
@@ -18,5 +29,4 @@ async def main(request):
 
 
 if __name__ == '__main__':
-    Base.metadata.create_all(bind=engine)  # Base에 연결된 모든 테이블 매핑
     APP.run(host='localhost', port=8000)
