507622eb5484733e17b40656cb3d5ada67bfc409
huskyhoochu <dfg1499@gmail.com>
2018-09-09 20:28:03 +0900

    [#2] 유저 삭제 메서드 구현

--- src/user/api.py
+++ src/user/api.py
@@ -1,6 +1,8 @@
 from sanic.views import HTTPMethodView
 from sanic.response import json
 
+from sqlalchemy_utils import Password
+
 from common.database import db_session
 
 from .model import User
@@ -24,4 +26,25 @@ class UserView(HTTPMethodView):
         db_session.flush()
         result = db_session.query(User).filter_by(email=email).first()
 
-        return json({'email': result.email, 'password': result.password})
+        return json(
+            {
+                'email': result.email,
+                'password': result.password
+            }, status=201)
+
+    async def delete(self, request):
+        """
+        회원 탈퇴
+        """
+        email = request.json['email']
+        password = request.json['password']
+
+        user = db_session.query(User).filter_by(email=email).first()
+
+        if user.password == password:
+            db_session.delete(user)
+            db_session.commit()
+            db_session.flush()
+            return json(None, status=204)
+
+        return json({'message': '유저 정보가 맞지 않습니다.'}, status=400)
