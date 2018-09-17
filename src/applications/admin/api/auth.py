0750595896e956244045a8244b3b516714d8e14b
huskyhoochu <dfg1499@gmail.com>
2018-09-17 20:02:05 +0900

    [#4] admin에 대한 JWT authentication 적용

---
+++ src/applications/admin/api/auth.py
@@ -0,0 +1,36 @@
+from sanic_jwt import exceptions
+
+from common.database import db_session
+from common.validation import Validation
+from applications.user import EXCEPTION_MESSAGE
+from ..model import Admin
+
+
+async def authenticate(request, *args, **kwargs):
+    """
+    로그인 시 유저 정보가 담긴 JWT 를 전달해주는 메서드
+    """
+    data = request.json
+
+    is_full = Validation.empty_validation(data)
+    if is_full is False:
+        raise exceptions.AuthenticationFailed(EXCEPTION_MESSAGE['empty_value'])
+
+    query_user = Validation.none_validation(db_session, Admin, data['username'])
+    if query_user is None:
+        raise exceptions.AuthenticationFailed(EXCEPTION_MESSAGE['none_user'])
+
+    if query_user.password != data['password']:
+        raise exceptions.AuthenticationFailed(EXCEPTION_MESSAGE['invalid_password'])
+
+    return query_user
+
+
+async def payload_extender(payload, user):
+    """
+    JWT 의 payload 를 확장하는 함수
+    """
+    data = user.to_dict()
+    payload.update(**data)
+
+    return payload
