f2973fe0118576f4c83410bbe785ef05e5636e31
huskyhoochu <dfg1499@gmail.com>
2018-09-17 19:55:53 +0900

    [#4] UserView, AdminView를 blueprint로 처리
    - sanic-jwt가 두 module에 각각 다른 authentication을 부여하도록

--- src/applications/admin/api/admin.py
+++ src/applications/admin/api/admin.py
@@ -4,19 +4,18 @@ from sanic.response import json
 
 from common.database import db_session
 from common.validation import Validation
-from .. import SUCCEED_MESSAGE, EXCEPTION_MESSAGE
-from ..model import User
+from applications.user import SUCCEED_MESSAGE, EXCEPTION_MESSAGE
+from ..model import Admin
 
-blueprint = Blueprint('User')
+blueprint = Blueprint('Admin')
 
 
-class UserView(HTTPMethodView):
+class AdminView(HTTPMethodView):
     """
     유저 관련 메서드 집합
     1. POST: 회원 가입
-    2. PUT: 정보 수정
-    3. PATCH: 비밀번호 수정
-    4. DELETE: 회원 탈퇴
+    2. PATCH: 비밀번호 수정
+    3. DELETE: 회원 탈퇴
     """
 
     async def post(self, request):
@@ -29,53 +28,18 @@ class UserView(HTTPMethodView):
         if is_full is False:
             return json({'message': EXCEPTION_MESSAGE['empty_value']}, status=400)
 
-        phone_length = Validation.phone_validation(data['phone'])
-        if phone_length is False:
-            return json({'message': EXCEPTION_MESSAGE['invalid_phone']}, status=400)
-
-        user = User(**data)
+        user = Admin(**data)
         db_session.add(user)
         db_session.commit()
         db_session.flush()
         db_session.close()
 
-        query_user = Validation.none_validation(db_session, User, data['username'])
+        query_user = Validation.none_validation(db_session, Admin, data['username'])
 
         return json({
             'username': query_user.username,
-            'email': query_user.email,
-            'phone': query_user.phone
+            'is_admin': query_user.is_admin,
         }, status=201)
-
-    async def put(self, request):
-        """
-        회원 정보 수정
-        """
-        data = request.json
-
-        is_full = Validation.empty_validation(data)
-        if is_full is False:
-            return json({'message': EXCEPTION_MESSAGE['empty_value']}, status=400)
-
-        phone_length = Validation.phone_validation(data['phone'])
-        if phone_length is False:
-            return json({'message': EXCEPTION_MESSAGE['invalid_phone']}, status=400)
-
-        query_user = Validation.none_validation(db_session, User, data['username'])
-        if query_user is None:
-            return json({'message': EXCEPTION_MESSAGE['none_user']}, status=400)
-
-        query_user.email = data['email']
-        query_user.phone = data['phone']
-
-        db_session.commit()
-        db_session.flush()
-
-        return json({
-            'username': query_user.username,
-            'email': query_user.email,
-            'phone': query_user.phone
-        }, status=200)
 
     async def patch(self, request):
         """
@@ -87,7 +51,7 @@ class UserView(HTTPMethodView):
         if is_full is False:
             return json({'message': EXCEPTION_MESSAGE['empty_value']}, status=400)
 
-        query_user = Validation.none_validation(db_session, User, data['username'])
+        query_user = Validation.none_validation(db_session, Admin, data['username'])
         if query_user is None:
             return json({'message': EXCEPTION_MESSAGE['none_user']}, status=400)
 
@@ -108,7 +72,7 @@ class UserView(HTTPMethodView):
         if is_full is False:
             return json({'message': EXCEPTION_MESSAGE['empty_value']}, status=400)
 
-        query_user = Validation.none_validation(db_session, User, data['username'])
+        query_user = Validation.none_validation(db_session, Admin, data['username'])
         if query_user is None:
             return json({'message': EXCEPTION_MESSAGE['none_user']}, status=400)
 
@@ -123,4 +87,4 @@ class UserView(HTTPMethodView):
         return json(None, status=204)
 
 
-blueprint.add_route(UserView.as_view(), '/user', strict_slashes=True)
+blueprint.add_route(AdminView.as_view(), '/admin', strict_slashes=True)
