419496de611626c7cbd8da95f24343a770771557
huskyhoochu <dfg1499@gmail.com>
2018-09-19 21:34:14 +0900

    [#5] survey model 및 api 생성

---
+++ src/applications/survey/api/question.py
@@ -0,0 +1,42 @@
+from sanic import Blueprint
+from sanic.views import HTTPMethodView
+from sanic.response import json
+
+from common.database import db_session
+from common.validation import Validation
+from common.messages import EXCEPTION_MESSAGE
+from ..model import Question
+
+blueprint = Blueprint('SurveyQuestion')
+
+
+class SurveyQuestionView(HTTPMethodView):
+    """
+    설문조사: 질문 메서드 집합
+    1. POST: 질문 생성
+    """
+
+    async def post(self, request):
+        """
+        질문 생성
+        """
+        data = request.json
+
+        is_full = Validation.empty_validation(data)
+        if is_full is False:
+            return json({'message': EXCEPTION_MESSAGE['empty_value']}, status=400)
+
+        question = Question(**data)
+        db_session.add(question)
+        db_session.commit()
+        db_session.flush()
+        db_session.close()
+
+        query_question = Validation.none_validation(db_session, Question, title=data['title'])
+
+        return json({
+            'title': query_question.title
+        }, status=201)
+
+
+blueprint.add_route(SurveyQuestionView.as_view(), '/survey/question', strict_slashes=True)
---
+++ src/applications/survey/model.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from common.database import Base
+
+
+class Question(Base):
+    __tablename__ = 'question'
+    id = Column(Integer, primary_key=True)
+    subject = Column(String(length=10), unique=True, nullable=False)
+    title = Column(String(length=255), unique=False, nullable=False)
+    selection = relationship('Selection', back_populates='questions')
+
+    def __repr__(self):
+        return f'<Question title={self.subject}>'
+
+
+class Selection(Base):
+    __tablename__ = 'selection'
+    id = Column(Integer, primary_key=True)
+    select = Column(String(length=255), unique=False, nullable=False)
+    question_id = Column(Integer, ForeignKey('question.id'))
+    question = relationship('Question', back_populates='selections')
