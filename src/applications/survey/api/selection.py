71fd0c856a1f6529f8e939e9feb37b3b8df107c0
huskyhoochu <dfg1499@gmail.com>
2018-10-01 15:24:29 +0900

    [#6] Selection 클래스 기반 뷰 제거

--- src/applications/survey/api/selection.py
+++ src/applications/survey/api/selection.py
@@ -1,100 +1,97 @@
 from sanic import Blueprint
-from sanic.views import HTTPMethodView
 from sanic.response import json
 
 from common.database import db_session
-from common.validation import empty_validation, query_validation, length_validation
+from common.validation import empty_validation, query_validation
 from common.messages import EXCEPTION_MESSAGE
 from ..model import Question, Selection
 
 blueprint = Blueprint('SurveySelection')
 
 
-class SelectionCreateListView(HTTPMethodView):
+@blueprint.route('/survey/question/<subject>/selection', methods=['OPTIONS', 'GET'], strict_slashes=True)
+async def get(request, subject):
+    """
+    선택지 호출
+    """
+    query_question = query_validation(db_session, Question, subject=subject)
+    if query_question is None:
+        return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
 
-    async def get(self, request, subject):
-        """
-        선택지 호출
-        """
-        query_question = query_validation(db_session, Question, subject=subject)
-        if query_question is None:
-            return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
+    result = {
+        'length': len(query_question.selection),
+        'items': []
+    }
 
-        result = {
-            'length': len(query_question.selection),
-            'items': []
+    for selection in query_question.selection:
+        item = {
+            'id': selection.id,
+            'select': selection.select
         }
+        result['items'].append(item)
 
-        for selection in query_question.selection:
-            item = {
-                'id': selection.id,
-                'select': selection.select
-            }
-            result['items'].append(item)
-
-        return json(result, status=200)
-
-    async def post(self, request, subject):
-        """
-        선택지 생성
-        """
-        data = request.json
-
-        is_full = empty_validation(data)
-        if is_full is False:
-            return json({'message': EXCEPTION_MESSAGE['empty_value']}, status=400)
-
-        query_question = query_validation(db_session, Question, subject=subject)
-        if query_question is None:
-            return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
-
-        query_question.selection.append(Selection(**data))
-        db_session.add(query_question)
-        db_session.commit()
-        db_session.flush()
-
-        return json({
-            'question_subject': query_question.subject,
-            'id': query_question.selection[-1].id,
-            'select': query_question.selection[-1].select
-        }, status=201)
+    return json(result, status=200)
 
 
-class SelectionRetrieveUpdateDestroyView(HTTPMethodView):
-    async def get(self, request, subject, id):
-        """
-        선택지 디테일
-        """
-        query_selection = db_session.query(Selection). \
-            filter(Question.subject == subject). \
-            filter_by(id=id).one()
-        if query_selection is None:
-            return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
+@blueprint.route('/survey/question/<subject>/selection', methods=['POST'], strict_slashes=True)
+async def post(request, subject):
+    """
+    선택지 생성
+    """
+    data = request.json
 
-        return json({
-            'question_subject': query_selection.question.subject,
-            'id': query_selection.id,
-            'select': query_selection.select
-        }, status=200)
+    is_full = empty_validation(data)
+    if is_full is False:
+        return json({'message': EXCEPTION_MESSAGE['empty_value']}, status=400)
 
-    async def delete(self, request, subject, id):
-        """
-        선택지 삭제
-        """
-        query_selection = db_session.query(Selection). \
-            filter(Question.subject == subject). \
-            filter_by(id=id).one()
-        if query_selection is None:
-            return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
+    query_question = query_validation(db_session, Question, subject=subject)
+    if query_question is None:
+        return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
 
-        db_session.delete(query_selection)
-        db_session.commit()
-        db_session.flush()
-        db_session.close()
+    query_question.selection.append(Selection(**data))
+    db_session.add(query_question)
+    db_session.commit()
+    db_session.flush()
 
-        return json(None, status=204)
+    return json({
+        'question_subject': query_question.subject,
+        'id': query_question.selection[-1].id,
+        'select': query_question.selection[-1].select
+    }, status=201)
 
 
-blueprint.add_route(SelectionCreateListView.as_view(), '/survey/question/<subject>/selection', strict_slashes=True)
-blueprint.add_route(SelectionRetrieveUpdateDestroyView.as_view(), '/survey/question/<subject>/selection/<id>',
-                    strict_slashes=True)
+@blueprint.route('/survey/question/<subject>/selection/<id>', methods=['OPTIONS', 'GET'], strict_slashes=True)
+async def get(request, subject, id):
+    """
+    선택지 디테일
+    """
+    query_selection = db_session.query(Selection). \
+        filter(Question.subject == subject). \
+        filter_by(id=id).one()
+    if query_selection is None:
+        return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
+
+    return json({
+        'question_subject': query_selection.question.subject,
+        'id': query_selection.id,
+        'select': query_selection.select
+    }, status=200)
+
+
+@blueprint.route('/survey/question/<subject>/selection/<id>', methods=['DELETE'], strict_slashes=True)
+async def delete(request, subject, id):
+    """
+    선택지 삭제
+    """
+    query_selection = db_session.query(Selection). \
+        filter(Question.subject == subject). \
+        filter_by(id=id).one()
+    if query_selection is None:
+        return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
+
+    db_session.delete(query_selection)
+    db_session.commit()
+    db_session.flush()
+    db_session.close()
+
+    return json(None, status=204)
