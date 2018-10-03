483c2643d068f8d346b4f6222ebb6f7e871334fa
huskyhoochu <dfg1499@gmail.com>
2018-10-03 17:28:45 +0900

    [#6] 선택지 수정 메서드 생성

--- src/applications/survey/api/selection.py
+++ src/applications/survey/api/selection.py
@@ -78,6 +78,34 @@ async def get(request, subject, id):
     }, status=200)
 
 
+@blueprint.route('/survey/question/<subject>/selection/<id>', methods=['PUT'], strict_slashes=True)
+async def put(request, subject, id):
+    """
+    선택지 수정
+    """
+    data = request.json
+
+    is_full = empty_validation(data)
+    if is_full is False:
+        return json({'message': EXCEPTION_MESSAGE['empty_value']}, status=400)
+
+    query_selection = db_session.query(Selection). \
+        filter(Question.subject == subject). \
+        filter_by(id=id).one()
+    if query_selection is None:
+        return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
+
+    query_selection.select = data['select']
+    db_session.commit()
+    db_session.flush()
+
+    return json({
+        'question_subject': query_selection.question.subject,
+        'id': query_selection.id,
+        'select': query_selection.select
+    }, status=200)
+
+
 @blueprint.route('/survey/question/<subject>/selection/<id>', methods=['DELETE'], strict_slashes=True)
 async def delete(request, subject, id):
     """
