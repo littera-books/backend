e1f50759d734a14ef649b585b10123b369dd020b
huskyhoochu <dfg1499@gmail.com>
2018-10-09 20:26:14 +0900

    [#10] ResignSurvey에서 데이터 핸들링 방식 수정
    - formData를 받아올 때 겉에 달라붙는 리스트 꺾쇠를 벗겨냄
    - CORS를 위한 GET 메서드 생성

--- src/applications/survey/api/resign_survey.py
+++ src/applications/survey/api/resign_survey.py
@@ -9,24 +9,33 @@ from ..model import ResignSurvey
 blueprint = Blueprint('ResignSurvey')
 
 
-@blueprint.route('/survey/resign', methods=['OPTIONS', 'POST'], strict_slashes=True)
+@blueprint.route('/survey/resign', methods=['OPTIONS', 'GET'], strict_slashes=True)
+async def get(request):
+    return json(None, status=200)
+
+
+@blueprint.route('/survey/resign', methods=['POST'], strict_slashes=True)
 async def post(request):
     """
     탈퇴 설문 생성
     """
-    data = request.json
+    raw_data = request.form
 
-    is_full = empty_validation(data)
+    is_full = empty_validation(raw_data)
     if is_full is False:
         return json({'message': EXCEPTION_MESSAGE['empty_value']}, status=400)
 
-    resign_survey = ResignSurvey(**data)
+    raw_content_value = raw_data['content'][0]
+    result_data = {
+        'content': raw_content_value
+    }
+
+    resign_survey = ResignSurvey(**result_data)
     db_session.add(resign_survey)
     db_session.commit()
     db_session.flush()
-    db_session.close()
 
-    query_resign_survey = query_validation(db_session, ResignSurvey, content=data['content'])
+    query_resign_survey = query_validation(db_session, ResignSurvey, id=resign_survey.id)
     if query_resign_survey is None:
         return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
 
@@ -36,7 +45,22 @@ async def post(request):
     }, status=201)
 
 
-@blueprint.route('/survey/resign/<survey_id>', methods=['OPTIONS', 'DELETE'], strict_slashes=True)
+@blueprint.route('/survey/resign/<survey_id>', methods=['OPTIONS', 'GET'], strict_slashes=True)
+async def get(request, survey_id):
+    """
+    탈퇴 질문 디테일
+    """
+    query_resign_survey = query_validation(db_session, ResignSurvey, id=survey_id)
+    if query_resign_survey is None:
+        return json({'message': EXCEPTION_MESSAGE['none_question']}, status=400)
+
+    return json({
+        'id': query_resign_survey.id,
+        'content': query_resign_survey.content
+    }, status=200)
+
+
+@blueprint.route('/survey/resign/<survey_id>', methods=['DELETE'], strict_slashes=True)
 async def delete(request, survey_id):
     """
     탈퇴 질문 삭제
