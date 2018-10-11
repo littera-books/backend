9fe82e1c64108e1d4347e1de00f5081e25b4b9ee
huskyhoochu <dfg1499@gmail.com>
2018-10-11 13:50:46 +0900

    [#7] 메시지 리스트 GET 메서드 생성

--- src/applications/message/api.py
+++ src/applications/message/api.py
@@ -4,6 +4,7 @@ from sanic.response import json
 from common.database import db_session
 from common.messages import EXCEPTION_MESSAGE
 from common.validation import empty_validation, query_validation
+from applications.user.model import User
 from applications.admin.model import Admin
 from .model import Message
 
@@ -12,9 +13,28 @@ blueprint = Blueprint('Message')
 
 @blueprint.route('/message/<user_id>', methods=['OPTIONS', 'GET'], strict_slashes=True)
 async def get(request, user_id):
-    return json({
-        'id': user_id,
-    }, status=200)
+    """
+    메시지 리스트
+    """
+    query_user = query_validation(db_session, User, id=user_id)
+    if query_user is None:
+        return json({'message': EXCEPTION_MESSAGE['none_user']}, status=400)
+
+    result = {
+        'length': len(query_user.admin),
+        'items': []
+    }
+
+    for message in query_user.admin:
+        item = {
+            'id': message.id,
+            'body': message.body,
+            'created_at': message.created_at,
+        }
+
+        result['items'].append(item)
+
+    return json(result, status=200)
 
 
 @blueprint.route('/message/<user_id>', methods=['POST'], strict_slashes=True)
