288d7962de60125a1dd533caea847515193efb73
huskyhoochu <dfg1499@gmail.com>
2018-10-16 11:50:48 +0900

    [#9] 구독 여부에 따라 리턴 값 분기

--- src/applications/user/api/user.py
+++ src/applications/user/api/user.py
@@ -85,6 +85,17 @@ async def get(request, user_id):
     if query_user is None:
         return json({'message': EXCEPTION_MESSAGE['none_user']}, status=400)
 
+    if query_user.subscription is None:
+        return json({
+            'id': query_user.id,
+            'first_name': query_user.first_name,
+            'last_name': query_user.last_name,
+            'address': query_user.address,
+            'phone': query_user.phone,
+            'email': query_user.email,
+            'subscription': ''
+        }, status=200)
+
     return json({
         'id': query_user.id,
         'first_name': query_user.first_name,
@@ -92,6 +103,7 @@ async def get(request, user_id):
         'address': query_user.address,
         'phone': query_user.phone,
         'email': query_user.email,
+        'subscription': query_user.subscription.product.description
     }, status=200)
 
 
